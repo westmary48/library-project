e8d7fc2911e3d750d382b8497de66115133ed273
Mary West <westmarye1219@gmail.com>
2019-09-19 11:21:06 -0500

    added form

--- libraryapp/views/books/list.py
+++ libraryapp/views/books/list.py
@@ -36,23 +36,23 @@ def book_list(request):
         return render(request, template, context)
 
 
-    # elif request.method == 'POST':
-    #     form_data = request.POST
+    elif request.method == 'POST':
+        form_data = request.POST
 
-    # with sqlite3.connect(Connection.db_path) as conn:
-    #     db_cursor = conn.cursor()
+    with sqlite3.connect(Connection.db_path) as conn:
+        db_cursor = conn.cursor()
 
-    #     db_cursor.execute("""
-    #     INSERT INTO libraryapp_book
-    #     (
-    #         title, author, isbn,
-    #         year_published, location_id, librarian_id
-    #     )
-    #     VALUES (?, ?, ?, ?, ?, ?)
-    #     """,
-    #     (form_data['title'], form_data['author'],
-    #         form_data['isbn'], form_data['year_published'],
-    #         request.user.librarian.id, form_data["location"]))
+        db_cursor.execute("""
+        INSERT INTO libraryapp_book
+        (
+            bookTitle, Author, ISBNNumber,
+            YearPublished, location_id, librarian_id
+        )
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (form_data['title'], form_data['author'],
+            form_data['isbn'], form_data['year_published'],
+            request.user.librarian.id, form_data["location"]))
 
-    #     return redirect(reverse('libraryapp:books'))
+        return redirect(reverse('libraryapp:books'))
 
