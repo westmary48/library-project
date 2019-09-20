dba5ae0e9a41b1a427cdef4e25bf0023bc8205a9
Mary West <westmarye1219@gmail.com>
2019-09-20 14:41:02 -0500

    wip

--- libraryapp/views/books/details.py
+++ libraryapp/views/books/details.py
@@ -2,26 +2,67 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from libraryapp.models import Book, Library
+from libraryapp.models import Book, Library, Librarian
 from libraryapp.models import model_factory
 from ..connection import Connection
 
 
+
+
+
+# def get_book(book_id):
+#     with sqlite3.connect(Connection.db_path) as conn:
+#         conn.row_factory = create_book
+#         db_cursor = conn.cursor()
+
+#         db_cursor.execute("""
+#         SELECT
+#             b.id book_id,
+#             b.title,
+#             b.isbn,
+#             b.author,
+#             b.year_published,
+#             b.librarian_id,
+#             b.location_id,
+#             li.id librarian_id,
+#             u.first_name,
+#             u.last_name,
+#             loc.id library_id,
+#             loc.title library_name
+#         FROM libraryapp_book b
+#         JOIN libraryapp_librarian li ON b.librarian_id = li.id
+#         JOIN libraryapp_library loc ON b.location_id = loc.id
+#         JOIN auth_user u ON u.id = li.user_id
+#         WHERE b.id = ?
+#         """, (book_id,))
+
+#         return db_cursor.fetchone()
+
+
+
 def get_book(book_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Book)
+        conn.row_factory = create_book
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
         SELECT
-            b.id,
+            b.id book_id,
             b.bookTitle,
             b.ISBNNumber,
             b.Author,
             b.YearPublished,
             b.librarian_id,
-            b.location_id
+            b.location_id,
+            li.id librarian_id,
+            u.first_name,
+            u.last_name,
+            loc.id library_id,
+            loc.title library_name
         FROM libraryapp_book b
+        JOIN libraryapp_librarian li ON b.librarian_id = li.id
+        JOIN libraryapp_library loc ON b.location_id = loc.id
+        JOIN auth_user u ON u.id = li.user_id
         WHERE b.id = ?
         """, (book_id,))
 
@@ -76,3 +117,28 @@ def book_details(request, book_id):
                 """, (book_id,))
 
             return redirect(reverse('libraryapp:books'))
+
+
+def create_book(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    book = Book()
+    book.id = _row["book_id"]
+    book.Author = _row["Author"]
+    book.ISBNNumber = _row["ISBNNumber"]
+    book.bookTitle = _row["bookTitle"]
+    book.YearPublished = _row["YearPublished"]
+
+    librarian = Librarian()
+    librarian.id = _row["librarian_id"]
+    librarian.first_name = _row["first_name"]
+    librarian.last_name = _row["last_name"]
+
+    library = Library()
+    library.id = _row["library_id"]
+    library.title = _row["library_name"]
+
+    book.librarian = librarian
+    book.location = library
+
+    return book
