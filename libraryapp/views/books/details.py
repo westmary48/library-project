f27a1ddf9aff3af426ff31cbd0b57df90582589a
Mary West <westmarye1219@gmail.com>
2019-09-20 15:49:00 -0500

    error went away

--- libraryapp/views/books/details.py
+++ libraryapp/views/books/details.py
@@ -7,39 +7,6 @@ from libraryapp.models import model_factory
 from ..connection import Connection
 
 
-
-
-
-# def get_book(book_id):
-#     with sqlite3.connect(Connection.db_path) as conn:
-#         conn.row_factory = create_book
-#         db_cursor = conn.cursor()
-
-#         db_cursor.execute("""
-#         SELECT
-#             b.id book_id,
-#             b.title,
-#             b.isbn,
-#             b.author,
-#             b.year_published,
-#             b.librarian_id,
-#             b.location_id,
-#             li.id librarian_id,
-#             u.first_name,
-#             u.last_name,
-#             loc.id library_id,
-#             loc.title library_name
-#         FROM libraryapp_book b
-#         JOIN libraryapp_librarian li ON b.librarian_id = li.id
-#         JOIN libraryapp_library loc ON b.location_id = loc.id
-#         JOIN auth_user u ON u.id = li.user_id
-#         WHERE b.id = ?
-#         """, (book_id,))
-
-#         return db_cursor.fetchone()
-
-
-
 def get_book(book_id):
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = create_book
